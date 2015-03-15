eb9d584c1c6a6aa9e04d96bae971bcc18fe08972
Kevin Zhang <zhangk6@rpi.edu>
2015-03-15 13:43:47 -0400

    Add choice.py

---
+++ Python/choice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Choice (boys)
+# TODO: quote handling
+import sys, random, socket
+
+# handle bad cases
+if(len(sys.argv) == 1):
+    print("Usage: choice FILE...")
+    sys.exit()
+
+# yo ansi boys
+UNDERLINE = '\033[4m'
+END = '\033[0m'
+
+# don't need the first argument...
+sys.argv.pop(0)
+
+# variables
+choices = len(sys.argv)
+chii = "chii@" + UNDERLINE + socket.gethostname() + END + ">"
+choice = sys.argv[random.randrange(0,choices)] + ","
+
+# get rid of whitespace
+chii.replace(" ","")
+choice.replace(" ","")
+
+# make a choice!
+print(chii,choice," obviously!")
