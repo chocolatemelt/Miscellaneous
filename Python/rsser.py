6c377fa93456dd1d214377bf889723f57fb8adc0
Kevin Zhang <kzhaaang@gmail.com>
2015-06-10 14:05:43 -0400

    Retire rsser.py

--- Python/rsser.py
+++ Python/rsser.py
@@ -3,6 +3,8 @@
 # Downloads from RSS via urllib
 # Requires feedparser python package.
 # Checks for the feed items <link> and <title>.
+# NOTE: Retired as of 6/10/15. This doesn't work well and the Bash script nyaa
+# in this repository works better and faster.
 import feedparser, urllib, sys
 
 # handle bad cases
