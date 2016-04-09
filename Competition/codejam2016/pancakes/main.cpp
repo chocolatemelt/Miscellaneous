311238d3d00850fae5c288ca3fd446a17ddfe5c9
Kevin Zhang <kzhaaang@gmail.com>
2016-04-08 23:14:22 -0400

    ff

---
+++ Competition/codejam2016/pancakes/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+
+int main() {
+	int n;
+	std::cin >>n;
+	if(n < 1 || n > 100) {
+		std::cerr << "invalid input size\n";
+		return 1;
+	}
+	int i = 0;
+	while(i < n) {
+	}
+	return 0;
+}
