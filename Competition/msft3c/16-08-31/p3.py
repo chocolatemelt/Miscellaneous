1a51c7e1dcdff147d24e578120ad852c96fe40a7
Kevin Zhang <kzhaaang@gmail.com>
2016-08-31 22:34:17 -0400

    rearrange

--- Competition/msft3c/16-08-31/p3.py
+++ Competition/msft3c/16-08-31/p3.py
@@ -1,11 +1,36 @@
-import networkx as nx
-from networkx import *
+inf = open('p2', 'r')
 
-inf = input('p3','r')
+row1 = []
+row2 = inf.readline().split()
 
-input_num = inf.readline()
+colordict = {
+        'red':['red'],
+        'blue':['blue'],
+        'yellow':['yellow'],
+        'purple':['red','blue'],
+        'orange':['red','yellow'],
+        'green':['blue','yellow'],
+        'blank':[]
+        }
 
-for i in input_num:
-    output = "Case #" + (i + 1) + ":"
-    words = inf.readline().split()
+last_color = 'blank'
+output = ''
+for i in range(0,len(row2)-1):
+    c1 = colordict[row2[i]]
+    c2 = colordict[row2[i+1]]
+    common = []
+    found = False
+    for color in c1:
+        if color in c2:
+            common.append(color)
+    for color in common:
+        if color != last_color:
+            output += color + ' '
+            last_color = color
+            found = True
+            break;
+    if not found:
+        output += 'blank '
+        last_color = 'blank'
 
+print(output)
--- Competition/msft3c/16-08-31/p4.py
+++ Competition/msft3c/16-08-31/p4.py
@@ -1,46 +1,11 @@
 import networkx as nx
 from networkx import *
 
-def generate_graph(words):
-    from string import ascii_lowercase as lowercase
-    G = nx.Graph(name="words")
-    lookup = dict((c,lowercase.index(c)) for c in lowercase)
-    def edit_distance_one(word):
-        for i in range(len(word)):
-            left, c, right = word[0:i], word[i], word[i+1:]
-            j = lookup[c] # lowercase.index(c)
-            for cc in lowercase[j+1:]:
-                yield left + cc + right
-    candgen = ((word, cand) for word in sorted(words)
-               for cand in edit_distance_one(word) if cand in words)
-    G.add_nodes_from(words)
-    for word, cand in candgen:
-        G.add_edge(word, cand)
-    return G
+inf = input('p3','r')
 
+input_num = inf.readline()
 
-inf = open('p4','r')
+for i in input_num:
+    output = "Case #" + (i + 1) + ":"
+    words = inf.readline().split()
 
-input_num = inf.readline().split()
-dict_num = input_num[0]
-queries = input_num[1]
-words = set()
-
-for i in range(0, int(dict_num)):
-    words.add(inf.readline().strip())
-
-for i in range(0, int(queries)):
-    line = inf.readline().split()
-    start = line[0]
-    end = line[1]
-    if start not in words:
-        print("-1")
-    elif end not in words:
-        print("-1")
-    else:
-        G = generate_graph(words)
-        try:
-            sp = shortest_path(G, start, end)
-            print(len(sp)-1)
-        except nx.NetworkXNoPath:
-            print("-1")
