b8819e8943295867126d5c3ba75821c49128ba5e
Kevin Zhang <kzhaaang@gmail.com>
2016-10-11 01:01:43 -0400

    finish matching

--- C++/matching-strings/matching.cpp
+++ C++/matching-strings/matching.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <stack>
+#include <fstream>
+
+int findMatchingPairs(std::string input) {
+	std::stack<char> stack;
+	char curChar = input[0];
+	int curIdx = 0;
+	while(curIdx < input.length()) {
+		if('a' <= curChar && curChar <= 'z') {
+			// lowercase
+			if(stack.empty()) return curIdx;
+			char match = stack.top();
+			stack.pop();
+			match = match - 'A' + 'a';
+			if(match != curChar) return curIdx;
+		}
+		else {
+			// uppercase
+			stack.push(curChar);
+		}
+		curChar = input[++curIdx];
+	}
+	return curIdx;
+}
 
 int main() {
+	std::ifstream inf("matching.txt");
+	std::string input;
+	while(inf >> input) {
+		std::cout << findMatchingPairs(input) << std::endl;
+	}
 	return 0;
 }
