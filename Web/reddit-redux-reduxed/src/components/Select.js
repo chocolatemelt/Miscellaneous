f92041206caacadc3b2ae0826acb8c2d955e3ca4
Kevin Zhang <kzhang@federatedwireless.com>
2017-07-31 16:42:42 -0400

    add initial components

---
+++ Web/reddit-redux-reduxed/src/components/Select.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Select extends Component {
+  render() {
+    <h1>subpbB</h1>
+  }
+}
+
+Select.propTypes = {
+}
--- Web/reddit-redux-reduxed/src/containers/AsyncApp.js
+++ Web/reddit-redux-reduxed/src/containers/AsyncApp.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {
 } from '../actions'
+import Picker from '../components/Select'
 
 class AsyncApp extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class AsyncApp extends Component {
     return(
       <div>
         <h1>subreddit</h1>
+        <Select />
       </div>
     )
   }
